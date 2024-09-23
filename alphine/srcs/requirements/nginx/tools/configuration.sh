35ebbbcfaa5a273c8df3519240fd3643905a6126
jungchoib <jungchoi1@naver.com>
2024-09-23 18:34:42 +0900

    final alpha

--- alphine/srcs/requirements/nginx/tools/configuration.sh
+++ alphine/srcs/requirements/nginx/tools/configuration.sh
@@ -3,8 +3,8 @@
 if [ ! -d "/etc/nginx/ssl" ]; then
     mkdir /etc/nginx/ssl
     openssl req -newkey rsa:4096 -days 365 -nodes -x509 \
-            -subj "/C=KR/ST=Seoul/L=Seoul/O=42Seoul/OU=Student/CN=junghych" \
-            -keyout /etc/nginx/ssl/junghych.key -out /etc/nginx/ssl/junghych.crt
+            -subj "/C=KR/ST=Seoul/L=Seoul/O=42Seoul/OU=Student/CN=seed" \
+            -keyout /etc/nginx/ssl/seed.key -out /etc/nginx/ssl/seed.crt
     mkdir -p /run/nginx
 fi
 
