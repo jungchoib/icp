2cb56143ea76a1b367fd61ae9ccde4a0de9edd25
jungchoib <jungchoi1@naver.com>
2024-09-24 20:02:21 +0900

    add docker outer network

--- default/srcs/requirements/mariadb/tools/configuration.sh
+++ default/srcs/requirements/mariadb/tools/configuration.sh
@@ -7,9 +7,9 @@ if [ ! -e "/run/mysqld/mysqld.sock" ]; then
     mysqld &
 
     #https://stackoverflow.com/questions/25503412/how-do-i-know-when-my-docker-mysql-container-is-up-and-mysql-is-ready-for-taking
-    while ! mysqladmin ping; do
-        sleep 1
-    done
+    # while ! mysqladmin ping; do
+    #     sleep 1
+    # done
 
     mysql -uroot --skip-password -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
     mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
